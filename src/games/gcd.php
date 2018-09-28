5dbb64738469dd5de3e4fc774e782fca60241462
aic513 <aic513@mail.ru>
2018-09-28 19:11:04 +0300

    add gcd

---
+++ src/games/gcd.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\Games\Gcd;
+
+use function BrainGames\Flow\gameFlow;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+function run()
+{
+    $gcd = function ($a, $b) use (&$gcd) {
+        $large = $a > $b ? $a : $b;
+        $small = $a > $b ? $b : $a;
+        $remainder = $large % $small;
+
+        return 0 === $remainder ? $small : $gcd($small, $remainder);
+    };
+    $infoAboutGame = function () use ($gcd) {
+        $first = rand(1, 100);
+        $second = rand(1, 100);
+
+        $question = "{$first} {$second}";
+        $answer = $gcd($first, $second);
+
+        return [$question, $answer];
+    };
+
+    gameFlow(DESCRIPTION, $infoAboutGame);
+}
