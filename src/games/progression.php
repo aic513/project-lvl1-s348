f0676850d8c53bfdead98f780a599d4c211674a0
aic513 <aic513@mail.ru>
2018-09-30 12:17:51 +0300

    fix edits

--- src/games/progression.php
+++ src/games/progression.php
@@ -6,6 +6,10 @@ use function BrainGames\Flow\gameFlow;
 
 const DESCRIPTION = 'What number is missing in this progression?';
 const LENGTH_OF_PROGRESSION = 10;
+const MIN_START_NUMBER = 1;
+const MAX_START_NUMBER = 30;
+const MIN_STEP_NUMBER = 2;
+const MAX_STEP_NUMBER = 10;
 
 function run()
 {
@@ -16,19 +20,18 @@ function run()
         return implode(' ', $question);
     };
 
-    $getProgression = function () {
-        $startNumber = rand(1, 30);
-        $step = rand(2, 10);
-        $endNumber = ($startNumber - $step) + $step * LENGTH_OF_PROGRESSION;
-
+    $getProgression = function ($startNumber, $step, $endNumber) {
         return range($startNumber, $endNumber, $step);
     };
 
     $generateGameData = function () use ($getQuestion, $getProgression) {
-        $progression = $getProgression();
+        $startNumber = rand(MIN_START_NUMBER, MAX_START_NUMBER);
+        $step = rand(MIN_STEP_NUMBER, MAX_STEP_NUMBER);
+        $endNumber = ($startNumber - $step) + $step * LENGTH_OF_PROGRESSION;
+        $progression = $getProgression($startNumber, $step, $endNumber);
         $missingIndex = array_rand($progression);
         $question = $getQuestion($progression, $missingIndex);
-        $answer = $progression[$missingIndex];
+        $answer = (string)$progression[$missingIndex];
 
         return [$question, $answer];
     };
